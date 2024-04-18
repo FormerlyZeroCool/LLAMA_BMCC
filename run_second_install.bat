a7fa5231dffc9611b5a09271b90ae37571bb3957
andrewrubinstein <andrew.rubinstein93@gmail.com>
2024-04-17 23:36:53 -0400

    remove auto yes because it seems not to work

--- run_second_install.bat
+++ run_second_install.bat
@@ -1,10 +1,10 @@
 #install ollama
-wsl yes "y" | sudo apt update && sudo apt upgrade
-wsl yes "y" | sudo apt install make
-wsl yes "y" | sudo apt install curl
-wsl yes "y" | sudo apt install git
-wsl yes "y" | sudo apt install g++
-wsl yes "y" | sudo apt install gcc
-wsl yes "y" | sudo apt install jq
+wsl sudo apt update && sudo apt upgrade
+wsl sudo apt install make
+wsl sudo apt install curl
+wsl sudo apt install git
+wsl sudo apt install g++
+wsl sudo apt install gcc
+wsl sudo apt install jq
 wsl curl -fsSL https://ollama.com/install.sh | sh
 
