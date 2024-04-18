f2a038483a75e92d9d6ff3a3cb91eb556001e65d
andrewrubinstein <andrew.rubinstein93@gmail.com>
2024-04-17 22:49:31 -0400

    add jq to installation script

--- install.sh
+++ install.sh
@@ -5,5 +5,6 @@ sudo apt update && sudo apt upgrade
  yes "y" | sudo apt install git
  yes "y" | sudo apt install g++
  yes "y" | sudo apt install gcc
+ yes "y" | sudo apt install jq
 curl -fsSL https://ollama.com/install.sh | sh
 
