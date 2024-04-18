38c34dcf06597a6ef827bb97ff1936dd5afa23de
andrewrubinstein <andrew.rubinstein93@gmail.com>
2024-04-17 23:33:29 -0400

    try to run all install commands from bat

--- run_second_install.bat
+++ run_second_install.bat
@@ -1,2 +1,10 @@
-wsl ./install.sh
-wsl
+#install ollama
+wsl sudo apt update && sudo apt upgrade
+wsl sudo apt install make
+wsl sudo apt install curl
+wsl sudo apt install git
+wsl sudo apt install g++
+wsl sudo apt install gcc
+wsl sudo apt install jq
+wsl curl -fsSL https://ollama.com/install.sh | sh
+
