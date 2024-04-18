8c3d7de9993f069f771961af86262ab4fe34fe4c
andrewrubinstein <andrew.rubinstein93@gmail.com>
2024-04-17 23:51:17 -0400

    try specifying distro to hope it carries through

--- run_second_install.bat
+++ run_second_install.bat
@@ -1,5 +1,5 @@
 #install ollama
-wsl bash -c "sudo apt update && sudo apt upgrade -y;
+wsl -d Debian bash -c "sudo apt update && sudo apt upgrade -y;
 sudo apt install -y make curl git g++ gcc jq;
 curl -fsSL https://ollama.com/install.sh | sudo sh"
 
