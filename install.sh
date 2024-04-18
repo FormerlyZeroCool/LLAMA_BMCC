92925d05e8ff34ccf7db5178260ee601d44550af
andrewrubinstein <andrew.rubinstein93@gmail.com>
2024-04-17 23:14:28 -0400

    remove auto yes to installation for debugging on windows

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #install ollama
 sudo apt update && sudo apt upgrade
- yes "y" | sudo apt install make
- yes "y" | sudo apt install curl
- yes "y" | sudo apt install git
- yes "y" | sudo apt install g++
- yes "y" | sudo apt install gcc
- yes "y" | sudo apt install jq
+sudo apt install make
+sudo apt install curl
+sudo apt install git
+sudo apt install g++
+sudo apt install gcc
+sudo apt install jq
 curl -fsSL https://ollama.com/install.sh | sh
 
