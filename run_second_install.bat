d48c9338e2c30fa12dd1007cc67a57441434da1e
andrewrubinstein <andrew.rubinstein93@gmail.com>
2024-04-17 23:48:17 -0400

    trying to add them into one bash incantation

--- run_second_install.bat
+++ run_second_install.bat
@@ -1,10 +1,5 @@
 #install ollama
-wsl sudo apt update && sudo apt upgrade
-wsl sudo apt install make
-wsl sudo apt install curl
-wsl sudo apt install git
-wsl sudo apt install g++
-wsl sudo apt install gcc
-wsl sudo apt install jq
-wsl curl -fsSL https://ollama.com/install.sh | sh
+wsl bash -c "sudo apt update && sudo apt upgrade -y;
+sudo apt install -y make curl git g++ gcc jq;
+curl -fsSL https://ollama.com/install.sh | sudo sh"
 
