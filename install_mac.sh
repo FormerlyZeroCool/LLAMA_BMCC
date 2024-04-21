0252e7811afee689eea55cbe471c5624334c76e1
noob339 <88294617+noob339@users.noreply.github.com>
2024-04-21 18:40:48 -0400

    mac script

---
+++ install_mac.sh
@@ -0,0 +1,9 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+sudo brew update && sudo brew upgrade
+sudo brew install make
+sudo brew install curl
+sudo brew install git
+sudo brew install g++
+sudo brew install gcc
+sudo brew install jq
+curl -fsSL https://ollama.com/install.sh | sh
